856d4756ef1b3d861c20959ee9eec0ae2a89f918
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-10-23 21:50:33 +0300

    added stud-list component

--- Angular/ui-358-master/src/app/app.module.ts
+++ Angular/ui-358-master/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudListComponent } from './students/stud-list/stud-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatExpansionModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/ui-358-master/src/app/students/stud-list/stud-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stud-list',
+  templateUrl: './stud-list.component.html',
+})
+export class StudListComponent {
+
+}
