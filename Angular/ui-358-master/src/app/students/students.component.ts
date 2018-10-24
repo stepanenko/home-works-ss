6b9a1ea0b7647cd6df81c11d6e358c2f16f90cfa
stepserg <serhiy.stepanenko@aiesec.net>
2018-10-24 15:27:24 +0300

    tour of heroes styles added

--- Angular/ui-358-master/src/app/students/students.component.ts
+++ Angular/ui-358-master/src/app/students/students.component.ts
@@ -8,27 +8,15 @@ import { STUDENTS } from '../mock-students';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+
   students = STUDENTS;
-
   selectedStudent: Student;
-  onSelect(student: Student): void {
-    this.selectedStudent = student;
-  }
-
-  // student: Student = {
-  //   id: 1,
-  //   name: 'Ken',
-  //   surname: 'Wilber',
-  //   phone: '+380971632212',
-  //   birthdate: '23.06.74',
-  //   address: 'New York',
-  //   group: 'Holly',
-  //   email: 'ken@wilber.com',
-  //   password: 'kenw'
-  // };
-
 
   constructor() {}
 
   ngOnInit() {}
+
+  onSelect(student: Student): void {
+    this.selectedStudent = student;
+  }
 }
