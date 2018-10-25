2e8174d6a62022a64f1cc61af32378e97b598653
stepserg <serhiy.stepanenko@aiesec.net>
2018-10-25 15:28:49 +0300

    changes

--- Angular/ui-358-master/src/app/app-routing.module.ts
+++ Angular/ui-358-master/src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { StudentsComponent } from './students/students.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/dashboard'
+
+  },
+  {
     path: 'students',
     component: StudentsComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
